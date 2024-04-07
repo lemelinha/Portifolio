dfdbc9de4e55ef72d77c061d9505ff6804c7bb6e
lemelinha <lucas.leme.0806@gmail.com>
2024-04-07 00:35:35 -0300

    cabeçalho do home

--- app/helpers/constants.php
+++ app/helpers/constants.php
@@ -3,6 +3,7 @@
 define('ROOT', dirname(__FILE__, 3) . '/');
 define('CONTROLLER_PATH', 'app\\controllers\\');
 define('VIEW_PATH', ROOT . 'app/views/');
+define('ASSETS', 'assets/');
 define('CSS', 'assets/css/');
 define('JS', 'assets/js/');
 define('IMAGES', 'assets/images/');
--- app/views/home.view.php
+++ app/views/home.view.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifólio</title>
     <link rel="stylesheet" href="<?= CSS . 'home/home.css?v=' . time() ?>">
+    <script src="https://kit.fontawesome.com/cdd96683ff.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?= password_hash("@Leme2112Portifolio", PASSWORD_DEFAULT) ?>
+    <header>
+        <img src="<?= ASSETS . 'personal/PFP.jfif' ?>" alt="Foto Pessoal">
+        <h1>Lucas Leme</h1>
+        <nav>
+            <a href="#" target="_blank"><i class="fa-brands fa-square-github fa-2xl"></i></a>
+            <a href="#" target="_blank"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+        </nav>
+    </header>
 </body>
 </html>
